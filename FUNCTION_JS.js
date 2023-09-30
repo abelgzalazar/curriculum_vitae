9601ac4243155df753bbc8bf6f7568031e3739c2
abelgzalazar <zalazarabelgines@gmail.com>
2023-09-30 16:40:23 -0300

    WIP: d57a777 curriculum_vitae_abel_zalazar

---
+++ FUNCTION_JS.js
@@ -0,0 +1,11 @@
+document.getElementById("titulo_direcciones").addEventListener("click", function() {
+    document.getElementById("contacts").classList.toggle("mostrar")})
+    document.getElementById("titulo_formacion").addEventListener("click", function() {
+        document.getElementById("formacion").classList.toggle("mostrar")})
+        document.getElementById("titulo_experiencia").addEventListener("click", function() {
+            document.getElementById("experiencia").classList.toggle("mostrar")})
+            document.getElementById("titulo_habilidades").addEventListener("click", function() {
+                document.getElementById("habilidades").classList.toggle("mostrar")})
+
+             
+
